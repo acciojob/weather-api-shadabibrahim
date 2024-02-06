27da80b02a913af43b348288551b00229fe9b61e
Mohd Shadab <89182232+shadabibrahim@users.noreply.github.com>
2024-02-07 01:25:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+let div = document.getElementById("weatherData");
+// alert
+let btn = document.createElement('button');
+let  p = document.createElement('p');
+btn.innerHTML = "Get Current Weather";
+div.appendChild(btn);
+let URL = "https://api.openweathermap.org/data/2.5/weather?q=london&units=metric&appid=65e6c064c2e37e1fc86b9fd9f7d9c701"
+
+async function getWeatherInfo(){
+	let response = await fetch(URL);
+	let data  = await response.json();
+	p.innerHTML = `<p>Current weather in London: ${data.weather[0].main}</p>`;
+	div.appendChild(p);
+}
+btn.addEventListener('click',getWeatherInfo);
