d42df1b4af09f7af7ee80b54db18e935cb3c056f
Mohd Shadab <89182232+shadabibrahim@users.noreply.github.com>
2024-02-07 01:32:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let btn = document.createElement('button');
 let  p = document.createElement('p');
 btn.innerHTML = "Get Current Weather";
 div.appendChild(btn);
-let URL = "https://api.openweathermap.org/data/2.5/weather?q=london&units=metric&appid=65e6c064c2e37e1fc86b9fd9f7d9c701"
+let URL = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=65e6c064c2e37e1fc86b9fd9f7d9c701"
 
 async function getCurrentWeather(){
 	let response = await fetch(URL);
